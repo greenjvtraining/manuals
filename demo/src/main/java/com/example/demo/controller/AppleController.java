135d1b89ee3f1e958db14b96001510f32ce2c8ef
cucomimak <cucomimak@gmail.com>
2024-05-01 03:35:49 +0900

    로그4제이 임포트

--- demo/src/main/java/com/example/demo/controller/AppleController.java
+++ demo/src/main/java/com/example/demo/controller/AppleController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.log4j.Log4j2;
+
 @Controller
 @RequestMapping("/apple")
 @Log4j2
